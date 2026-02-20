5789aca5190781d09c45d60cbd8f0c3accf3e5b1
Reality <dgfhigufufjghhhfuftfh@gmail.com>
2026-02-20 17:00:24 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,45 +1,54 @@
-// Clock
-function updateClock() {
-  const now = new Date();
-  document.getElementById("clock").innerText =
-    now.toLocaleTimeString();
+// Switch Forms
+function showRegister() {
+    document.getElementById("loginForm").style.display = "none";
+    document.getElementById("registerForm").style.display = "block";
 }
-setInterval(updateClock, 1000);
-updateClock();
 
-// Matches (demo data – FREE)
-const matches = [
-  {
-    home: "Real Madrid",
-    away: "Barcelona",
-    score: "2 - 1",
-    time: "75'"
-  },
-  {
-    home: "Man City",
-    away: "Arsenal",
-    score: "1 - 1",
-    time: "HT"
-  },
-  {
-    home: "Juventus",
-    away: "Inter",
-    score: "0 - 0",
-    time: "20'"
-  }
-];
+function showLogin() {
+    document.getElementById("registerForm").style.display = "none";
+    document.getElementById("loginForm").style.display = "block";
+}
 
-const container = document.getElementById("matches");
+// Register
+document.getElementById("registerForm").addEventListener("submit", function(e){
+e.preventDefault();
 
-matches.forEach(match => {
-  const div = document.createElement("div");
-  div.className = "match";
+let username = document.getElementById("regUsername").value;
+let email = document.getElementById("regEmail").value;
+let age = document.getElementById("regAge").value;
+let password = document.getElementById("regPassword").value;
 
-  div.innerHTML = `
-    <div class="teams">${match.home} vs ${match.away}</div>
-    <div class="score">${match.score}</div>
-    <div class="time">${match.time}</div>
-  `;
+localStorage.setItem("username", username);
+localStorage.setItem("email", email);
+localStorage.setItem("age", age);
+localStorage.setItem("password", password);
 
-  container.appendChild(div);
+alert("Account Created!");
+showLogin();
+});
+// Login
+document.getElementById("loginForm").addEventListener("submit", function(e){
+    e.preventDefault();
+
+    let username = document.getElementById("loginUsername").value;
+    let password = document.getElementById("loginPassword").value;
+
+    let savedUser = localStorage.getItem("username");
+    let savedPass = localStorage.getItem("password");
+
+    if(username === savedUser && password === savedPass){
+        localStorage.setItem("loggedUser", username);
+        window.location.href = "chat.html";
+    } else {
+        alert("Wrong Username or Password!");
+    }
+function sendMessage() {
+  const text = messageInput.value.trim();
+  if (text === '') return;
+
+  // استعمل النظام الجديد
+  addMessage(currentUser, text);
+
+  messageInput.value = '';
+}
 });
